e37cbcfc221460edd6ad27245167fe19451147cd
PaintableToast <rckimyon@yahoo.com>
2023-06-09 09:27:09 +0200

    GameStatus - initNewGame() isn't ready yet. GameStatus is a class to have the values of the actual Game
    GameStatus implements GameHandler
    GameHandler ist für @Kiril zum Updaten von Grafik. Und für @Viktor um die Advanced AI zu schreiben die ja alle Karten sehen muss.
    
    GameStatus ist noch nicht fertig und LocalPlayer braucht noch den ScoreManager und die Karten.

---
+++ src/bauernhof/app/launcher/GameHandler.java
@@ -0,0 +1,59 @@
+package bauernhof.app.launcher;
+
+import bauernhof.preset.Move;
+import bauernhof.preset.Player;
+import bauernhof.preset.card.Card;
+
+import java.util.Set;
+
+/**
+ * @author Ramon Cemil Kimyon
+ * @date 09.06.2023 01:05
+ */
+public interface GameHandler {
+    /**
+     * Deposited Cards on the table
+     * @return
+     */
+    public Set<Card> getDepositedCards();
+    /**
+     * Cards on Card stack
+     * @return
+     */
+    public Set<Card> getDrawPileCards();
+
+    /**
+     * get Players sorted by ID
+     * @return
+     */
+    public Set<Player> getPlayers();
+
+    /**
+     * move - Actual Player Move executed by ActualPlayer
+     * drawpile - checks if player gets cards by DrawPileCards
+     * @param move, drawpile
+     */
+    public void doMove(final Move move, final boolean drawpile) throws Exception;
+
+    /**
+     * gets the Player who has to play now
+     * @return
+     */
+    public Player getActualPlayer();
+
+    /**
+     * Returns the Actual Round Count.
+     * A Round is finished if the first Players has to play again.
+     * @return actual_roundcount;
+     */
+    public int getRound();
+
+    /**
+     * initNewGame();
+     */
+    public void initNewGame() throws Exception;
+    /**
+     * Mix all Cards
+     */
+    public void mixCards();
+}
---
+++ src/bauernhof/app/player/LocalPlayer.java
@@ -0,0 +1,84 @@
+package bauernhof.app.player;
+
+/**
+ * @author Ramon Cemil Kimyon
+ * @date 09.06.2023 00:55
+ */
+
+import bauernhof.preset.GameConfiguration;
+import bauernhof.preset.ImmutableList;
+import bauernhof.preset.Move;
+import bauernhof.preset.card.Card;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class LocalPlayer implements PlayerHandler {
+
+    private String name;
+    private ImmutableList<Card> initialDrawPile;
+    private GameConfiguration gameConfiguration;
+    private Move move;
+    private int numplayers, playerid;
+
+    public LocalPlayer(final String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String getName() throws Exception {
+        return this.name;
+    }
+
+    @Override
+    public void init(GameConfiguration gameConfiguration, ImmutableList<Card> initialDrawPile, int numplayers, int playerid) throws Exception {
+        this.gameConfiguration = gameConfiguration;
+        this.initialDrawPile = initialDrawPile;
+        this.numplayers = numplayers;
+        this.playerid = playerid;
+    }
+
+    @Override
+    public Move request() throws Exception {
+        return this.move;
+    }
+
+    @Override
+    public void update(Move opponentMove) throws Exception {
+        initialDrawPile.remove(opponentMove.getDeposited());
+        initialDrawPile.add(opponentMove.getTaken());
+        this.move = opponentMove;
+    }
+
+    @Override
+    public int getScore() throws Exception {
+        return 0;
+    }
+
+    @Override
+    public void verifyGame(ImmutableList<Integer> scores) throws Exception {
+
+    }
+
+    @Override
+    public Set<Card> getCards() {
+        final Set<Card> cards = new HashSet<>();
+        for (byte i = 0; i < cards.size(); i++) cards.add(initialDrawPile.get(0));
+        return cards;
+    }
+
+    @Override
+    public Set<Card> getActiveCards() {
+        return null;
+    }
+
+    @Override
+    public Set<Card> getBlockedCards() {
+        return null;
+    }
+
+    @Override
+    public int getPlayerID() {
+        return playerid;
+    }
+}
