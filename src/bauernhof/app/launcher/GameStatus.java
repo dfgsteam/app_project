582c8b216df642a6a5b1451366b880eec80d6a2f
PaintableToast <rckimyon@yahoo.com>
2023-06-09 09:50:52 +0200

    Updated GameStatus and executed to give Cards to player by executing the initNewGame() Method

--- src/bauernhof/app/launcher/GameStatus.java
+++ src/bauernhof/app/launcher/GameStatus.java
@@ -11,15 +11,14 @@ import bauernhof.preset.Move;
 import bauernhof.preset.Player;
 import bauernhof.preset.card.Card;
 
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.Set;
+import java.util.*;
 
 public class GameStatus implements GameHandler{
     private int round;
     private Player actual_player;
     private Iterator<Player> player_iterator;
-    private Set<Card> deposited_cards, drawpile_cards;
+    private Set<Card> deposited_cards;
+    private ArrayList<Card> drawpile_cards = new ArrayList<>();
     private Set<Player> players;
     private GameConfiguration configuration;
 
@@ -35,7 +34,8 @@ public class GameStatus implements GameHandler{
                       final Set<Card> drawpile_cards,
                       final Set<Card> deposited_cards) throws Exception {
         this.round = round;
-        this.drawpile_cards = drawpile_cards;
+        for (final Card card : drawpile_cards)
+            this.drawpile_cards.add(card);
         this.deposited_cards = deposited_cards;
         this.players = players;
         this.actual_player = player_iterator.next();
@@ -49,7 +49,7 @@ public class GameStatus implements GameHandler{
     }
 
     @Override
-    public Set<Card> getDrawPileCards() {
+    public ArrayList<Card> getDrawPileCards() {
         return this.drawpile_cards;
     }
 
@@ -60,7 +60,7 @@ public class GameStatus implements GameHandler{
 
     @Override
     public void doMove(final Move move, final boolean drawpile) throws Exception {
-        if (drawpile) drawpile_cards.remove(move.getTaken());
+        if (drawpile) removeTopCard();
         else deposited_cards.remove(move.getTaken());
         deposited_cards.add(move.getDeposited());
         getActualPlayer().update(move);
@@ -82,30 +82,28 @@ public class GameStatus implements GameHandler{
         return this.round;
     }
 
- /*   private void removeTopCard() {
-        this.drawpile_cards.remove();
+    private Card removeTopCard() {
+        final Card card = drawpile_cards.get(drawpile_cards.size() - 1);
+        this.drawpile_cards.remove(card);
+        return card;
     }
-    private void addCard(Card card) {
-        this.drawpile_cards.add(card);
-    }
-  */
 
     @Override
     public void initNewGame() throws Exception {
         byte count = 0;
-        final ImmutableList<Card> drawpilecard_list = new ImmutableList<>();
-        for (byte i = 0; i < configuration.getNumCardsPerPlayerHand(); i++) {
-            //removeCard(drawpilecard_list.get());
-        }
+        mixCards();
         for (final Player player : players) {
+            final ImmutableList<Card> drawpilecard_list = new ImmutableList<>();
+            for (byte i = 0; i < configuration.getNumCardsPerPlayerHand(); i++) {
+                drawpilecard_list.add(removeTopCard());
+            }
             player.init(configuration, drawpilecard_list, players.size(), count);
             count++;
         }
-
     }
 
     @Override
     public void mixCards() {
-
+        Collections.shuffle(drawpile_cards);
     }
 }
