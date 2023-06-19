98d9336cb2f68afc0c7c2b8fec985a6d691d5ec5
julius.hunold_uni <info@julius-hunold.de>
2023-06-20 00:32:59 +0200

    cards

--- src/bauernhof/app/GaCoPa.java
+++ src/bauernhof/app/GaCoPa.java
@@ -9,11 +9,11 @@ import java.io.*;
 import java.util.*;
 
 
-class CaGoPa implements GameConfigurationParser{
+class GaCoPa implements GameConfigurationParser{
     public static void main(String args[]) {
         try {
             // XML-Datei einlesen
-            File inputFile = new File("../../../config/cards.xml");
+            File inputFile = new File("cards.xml");
             DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
             DocumentBuilder builder = factory.newDocumentBuilder();
             Document document = builder.parse(inputFile);
@@ -45,6 +45,8 @@ class CaGoPa implements GameConfigurationParser{
             for (int i=0; i<cardColorElements.getLength(); i++) {
                 Element cardColorElement = (Element) cardColorElements.item(i);
                 //System.out.println("cardColor: " + cardColorElement.getTextContent() + " " + cardColorElement.getAttribute("color"));
+
+                //-> Erst Karten, dann Effekte und mit einem NEUEN Setter in Karte hinzufügen
         
                 if (!cardColors.containsKey(cardColorElement.getTextContent())) {
                     cardColors.put(cardColorElement.getTextContent(), new CardColor(cardColorElement.getTextContent(), cardColorElement.getAttribute("color")));
@@ -58,9 +60,49 @@ class CaGoPa implements GameConfigurationParser{
             NodeList cardsElements = root.getElementsByTagName("Card");
             System.out.println("cards: " + cardsElements.getLength());
 
+            Set<String> effectedCards;
+
             for (int i=0; i<cardsElements.getLength(); i++) {
                 Element cardElement = (Element) cardsElements.item(i);
+                
+                NodeList cardEffectElements = cardElement.getElementsByTagName("Effect");
+                for (int x=0; x<cardEffectElements.getLength(); x++) {
+                    Element cardEffectElement = (Element) cardEffectElements.item(x);
+                    System.out.println("effect type: " + cardEffectElement.getAttribute("type"));
+
+                    //Set<Either<Card,CardColor>> effectedCards;
+                    effectedCards.clear();
+                    NodeList effectElements = cardEffectElement.getElementsByTagName("CardColorRef");
+                    if (effectElements != null)
+                        for (int y=0; y<effectElements.getLength(); x++) {
+                            Element effectElement = (Element) effectElements.item(x);
+                            effectedCards.add(effectElement.getTextContent());
+                        }
+
+                    effectElements = cardEffectElement.getElementsByTagName("CardRef");
+                    if (effectElements != null)
+                        for (int y=0; y<effectElements.getLength(); x++) {
+                            Element effectElement = (Element) effectElements.item(x);
+                            effectedCards.add(effectElement.getTextContent());
+                        }
+                    System.out.println(effectedCards);
+
+                    if (effectedCards.isEmpty()) {
+                        System.out.println("ERROR: Effekt sind keine Karten zugeorndet!");
+                        System.exit(1);
+                    }
+                }
+
+
+
+                
                 System.out.println("card name: " + cardElement.getAttribute("name") + " " + cardColors.get(cardElement.getAttribute("color")).getAWTColor());
+            
+            
+            
+            
+            
+            
             }
 
 
