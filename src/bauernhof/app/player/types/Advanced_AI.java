d248849a91a1108ee5a046488237439954beed47
viktor.tevosyan <viktor.tevosyan@stud.uni-goettingen.de>
2023-06-22 23:16:28 +0200

    Simple and RandomAIs

---
+++ src/bauernhof/app/player/types/Advanced_AI.java
@@ -0,0 +1,39 @@
+package bauernhof.app.player.types;
+import bauernhof.app.launcher.GameBoardState;
+import bauernhof.app.player.AbstractGamePlayer;
+import bauernhof.preset.Move;
+import bauernhof.preset.PlayerType;
+import bauernhof.preset.card.Card;
+
+public class Advanced_AI extends AbstractGamePlayer implements AIHeader {
+
+    public Advanced_AI(String name, GameBoardState state, PlayerType type) {
+        super(name, state, type);
+        //TODO Auto-generated constructor stub
+    }
+
+    @Override
+    public Move calculateNextMove() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'calculateNextMove'");
+    }
+
+    @Override
+    public Card cardFromDeposit() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'cardFromDeposit'");
+    }
+
+    @Override
+    public Card cardFromStack() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'cardFromStack'");
+    }
+
+    @Override
+    public Card removeFromOwn() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'removeFromOwn'");
+    }
+    
+}
