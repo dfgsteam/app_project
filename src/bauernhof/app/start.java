b8057756ff9e27aca0a11908f78ebd4b834e09d8
julius.hunold_uni <info@julius-hunold.de>
2023-06-27 19:09:43 +0200

    main function

---
+++ src/bauernhof/app/start.java
@@ -0,0 +1,17 @@
+package bauernhof.app;
+
+import java.io.File;
+import java.io.IOException;
+
+import bauernhof.preset.*;
+
+public class start {
+    public static void main(String args[]) throws GameConfigurationException, IOException {
+        File gameConfFile = new File("config/cards.xml");
+        GameConfigurationParser GameConfPars = new GaCoPa();
+        GameConfiguration GaCo = GameConfPars.parse(gameConfFile);
+
+        System.out.println(GaCo.getConfigDescription());
+
+    }
+}
