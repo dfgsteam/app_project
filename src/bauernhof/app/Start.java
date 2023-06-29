ba4441b7785ec7aefc093e03abb0b7d3892216fb
julius.hunold_uni <info@julius-hunold.de>
2023-06-29 18:29:47 +0200

    new launcher

--- src/bauernhof/app/Start.java
+++ src/bauernhof/app/Start.java
@@ -3,15 +3,18 @@ package bauernhof.app;
 import java.io.File;
 import java.io.IOException;
 
+import bauernhof.app.ui.launcher.BaFr;
 import bauernhof.preset.*;
 
-public class start {
-    public static void main(String args[]) throws GameConfigurationException, IOException {
-        File gameConfFile = new File("gameconfigs/cards.xml");
+public class Start {
+    public static void main(String args[]) throws GameConfigurationException, IOException, InterruptedException {
+        File gameConfFile = new File("gameconfigs/bauernhof.xml");
         GameConfigurationParser GameConfPars = new GaCoPa();
         GameConfiguration GaCo = GameConfPars.parse(gameConfFile);
 
         System.out.println(GaCo.getConfigDescription());
+
+        BaFr sf = new BaFr();
 
     }
 }
