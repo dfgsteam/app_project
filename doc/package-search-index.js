4b78145a04d3d014bd241c5ae074566947d408f6
julius.hunold_uni <info@julius-hunold.de>
2023-06-29 18:30:01 +0200

    remove build + etc

--- doc/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"bauernhof.app"},{"l":"bauernhof.app.card"},{"l":"bauernhof.preset"},{"l":"bauernhof.preset.card"},{"l":"bauernhof.preset.networking"}]
---
+++ src/bauernhof/app/ui/game/SettingsPanel.java
@@ -0,0 +1,19 @@
+package bauernhof.app.ui.game;
+
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class SettingsPanel implements ActionListener {
+
+
+
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
+
+
+
