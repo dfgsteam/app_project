1afb8fdb20f94383c4bfc6f6e2fdf20718fc85c8
Florian Will <florian.will01@stud.uni-goettingen.de>
2023-07-05 20:18:49 +0200

    Netmain Test

--- src/bauernhof/app/Netzmain.java
+++ src/bauernhof/app/Netzmain.java
@@ -1,25 +1,75 @@
 package bauernhof.app;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
-public class Netzmain {
-    public static void main(String[] args) throws UnknownHostException, IOException{
-        String IP = "1111";
-        int Port = 11;
-        //int length = args.length;
-        //client to server
-        if(IP != null){
-            try (Socket ss = new Socket(IP, Port)) {
-            }
+/**
+ * This class was written by
+ * @author Florian Will
+ * @date 05.07.2023
+ */
 
+public class Netzmain {
+    public static void main(String[] args) throws UnknownHostException, IOException {
+        if (args.length < 1) {
+            System.out.println("Bitte geben Sie die Anzahl der Clients an, die verbunden werden sollen.");
+            return;
         }
 
-        else if(IP == null){
-            Socket ss = new Socket("localhost", Port);
-            //ss.accept();
-        }        
+        int numberOfClients = Integer.parseInt(args[0]);
+        int Port = 1409;
+
+        // Server
+        if (args.length == 2) {
+            try (ServerSocket serverSocket = new ServerSocket(Port)) {
+                System.out.println("Server wartet auf Verbindung...");
+
+                for (int i = 1; i <= numberOfClients; i++) {
+                    Socket clientSocket = serverSocket.accept();
+                    System.out.println("Client" + i + " erfolgreich mit dem Server verbunden.");
+
+                    // Nachricht vom Client empfangen
+                    BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                    String receivedMessage = in.readLine();
+                    System.out.println("Nachricht vom Client" + i + " erhalten: " + receivedMessage);
+
+                    // Nachricht an Client senden
+                    PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+                    out.println("Nachricht vom Server an Client" + i);
+                }
+            } catch (IOException e) {
+                System.out.println("Fehler beim Akzeptieren der Verbindung: " + e.getMessage());
+            }
+        }
+
+        // Client
+        if (args.length == 3) {
+            try (Socket ss = new Socket("127.0.0.1", Port)) {
+                System.out.println("Client erfolgreich mit dem Server verbunden.");
+
+                // Nachricht an Server senden
+                PrintWriter out = new PrintWriter(ss.getOutputStream(), true);
+
+                // Eingabeaufforderung für den Client
+                BufferedReader userInputReader = new BufferedReader(new InputStreamReader(System.in));
+                System.out.print("Nachricht eingeben: ");
+                String userInput = userInputReader.readLine();
+
+                out.println(userInput);
+
+                // Antwort vom Server empfangen
+                BufferedReader in = new BufferedReader(new InputStreamReader(ss.getInputStream()));
+                String receivedMessage = in.readLine();
+                System.out.println("Antwort vom Server: " + receivedMessage);
+
+            } catch (IOException e) {
+                System.out.println("Fehler beim Herstellen der Verbindung zum Server: " + e.getMessage());
+            }
+        }
     }
-    
 }
