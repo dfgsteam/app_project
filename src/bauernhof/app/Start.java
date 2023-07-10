4fe31e08d750cebd0e756d987de1fb346e030c6f
Florian Will <florian.will01@stud.uni-goettingen.de>
2023-07-11 00:08:58 +0200

    Start Netzwerk

--- src/bauernhof/app/Start.java
+++ src/bauernhof/app/Start.java
@@ -1,29 +1,48 @@
 package bauernhof.app;
 
 import java.io.File;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.ArrayList;
 
 import bauernhof.app.launcher.GameBoardState;
 import bauernhof.app.player.AbstractGamePlayer;
 import bauernhof.app.ui.game.GameBoard;
 import bauernhof.preset.*;
+import bauernhof.preset.networking.C2SConnection;
 
 import javax.swing.*;
 
 public class Start {
+
+    static int Port = 1409;
+    static int Network = 1;
+
     public static void main(String args[]) throws Exception {
-        File gameConfFile = new File("gameconfigs/bauernhof.xml");
-        GameConfigurationParser GameConfPars = new GaCoPa();
-        GameConfiguration GaCo = GameConfPars.parse(gameConfFile);
+        if(Network<1){
+            File gameConfFile = new File("gameconfigs/bauernhof.xml");
+            GameConfigurationParser GameConfPars = new GaCoPa();
+            GameConfiguration GaCo = GameConfPars.parse(gameConfFile);
 
-        GameBoardState gameBoardState = new GameBoardState(new String[]{"Florian", "Smila", "Alrun", "Lotta"}, new PlayerType[]{PlayerType.SIMPLE_AI, PlayerType.RANDOM_AI, PlayerType.RANDOM_AI, PlayerType.RANDOM_AI}, GaCo, new ImmutableList<>(GaCo.getCards()));
+            GameBoardState gameBoardState = new GameBoardState(new String[]{"Florian", "Smila", "Alrun", "Lotta"}, new PlayerType[]{PlayerType.SIMPLE_AI, PlayerType.RANDOM_AI, PlayerType.RANDOM_AI, PlayerType.RANDOM_AI}, GaCo, new ImmutableList<>(GaCo.getCards()));
 
-        GameBoard GB = new GameBoard(GaCo,gameBoardState);
-        setDefaultDesigns();
-        gameBoardState.initGame(GB);
+            GameBoard GB = new GameBoard(GaCo,gameBoardState);
+            setDefaultDesigns();
+            gameBoardState.initGame(GB);
 
-        System.out.println(GaCo.getConfigDescription());
-
+            System.out.println(GaCo.getConfigDescription());
+        }
+        else if(Network == 1){
+            ServerSocket socket = new ServerSocket(Port);
+            for(int i = 0; i < playernames.length; i++){
+                Socket client = socket.accept();
+            }
+        }
+        else if(Network == 2){
+            Socket socket = new Socket("localhost", Port);
+            C2SConnection(socket, GameConfigurationParser, "hi");
+            socket.handlePacket();
+        }
 
         //BaFr sf = new BaFr();
 
@@ -33,4 +52,6 @@ public class Start {
         JFrame.getFrames()[0].setExtendedState(JFrame.MAXIMIZED_BOTH);
         JFrame.getFrames()[0].setIconImage(new ImageIcon("graphics/bauernhof_logo.png").getImage());
     }
+
+
 }
