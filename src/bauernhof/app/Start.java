51abc4be4ef7a4f7c4186d61c50163ff6eadcb78
Florian Will <florian.will29@gmail.com>
2023-07-11 18:10:11 +0200

    Network - Connection

--- src/bauernhof/app/Start.java
+++ src/bauernhof/app/Start.java
@@ -22,11 +22,12 @@ import javax.swing.*;
 
 public class  Start {
     private static int client_connections = 1;
+
     private static int port = 5055;
     public static void main(String args[]) throws Exception {
         int network = Integer.valueOf(args[0]);
         if (args.length > 0)
-        network = Integer.valueOf(args[0]);
+            network = Integer.valueOf(args[0]);
             File gameConfFile = new File("gameconfigs/bauernhof.xml");
             GameConfigurationParser GameConfPars = new GaCoPa();
             GameConfiguration GaCo = GameConfPars.parse(gameConfFile);
@@ -49,55 +50,56 @@ public class  Start {
         }
     }
     private static final void initServer(final GameConfiguration configuration) throws Exception {
-        // SERVER
-        final ServerSocket server_socket = new ServerSocket(port);
-        final ArrayList<S2CConnection> s2cconnections = new ArrayList<>();
-        for (int i = 0; i < client_connections; i++) {
-            System.out.println("Wait for Client...");
-            s2cconnections.add(new S2CConnection(server_socket.accept()));
-            System.out.println("Client connected");
-        }
+        try (// SERVER
+        ServerSocket server_socket = new ServerSocket(port)) {
+            final ArrayList<S2CConnection> s2cconnections = new ArrayList<>();
+            for (int i = 0; i < client_connections; i++) {
+                System.out.println("Wait for Client...");
+                s2cconnections.add(new S2CConnection(server_socket.accept()));
+                System.out.println("Client connected");
+            }
 
-        final String[] playernames = new String[4];
-        playernames[0] = "Player 1";
-        playernames[1] = "Player 2";
-        playernames[2] = "Player 3";
-        playernames[3] = "HOFBAUERN";
-        PlayerType[] types = new PlayerType[playernames.length];
-        types[0] = PlayerType.RANDOM_AI;
-        types[1] = PlayerType.RANDOM_AI;
-        types[2] = PlayerType.RANDOM_AI;
-        types[3] = PlayerType.REMOTE;
+            final String[] playernames = new String[4];
+            playernames[0] = "Player 1";
+            playernames[1] = "Player 2";
+            playernames[2] = "Player 3";
+            playernames[3] = "HOFBAUERN";
+            PlayerType[] types = new PlayerType[playernames.length];
+            types[0] = PlayerType.RANDOM_AI;
+            types[1] = PlayerType.RANDOM_AI;
+            types[2] = PlayerType.RANDOM_AI;
+            types[3] = PlayerType.REMOTE;
 
-        /*for (int i = playernames.length - 1; i > playernames.length - 1 - client_connections; i--) {
-            playernames[i] = s2cconnections.get(i - (playernames.length - client_connections)).getProjectName();
-            System.out.println(s2cconnections.get(i - (playernames.length - client_connections)).getProjectName());
-            System.out.println(i);
-            System.out.println(i - (playernames.length - client_connections));
-        } */
-        for (final S2CConnection connection : s2cconnections) {
-            connection.setPlayerNames(new ImmutableList<>(Arrays.asList(playernames)));
-            for (final String name : new ImmutableList<>(Arrays.asList(playernames)))
-                System.out.println(name);
-            ImmutableList<Card> cards = new ImmutableList<>(configuration.getCards());
-            System.out.println(connection.getProjectName());
-            System.out.println(cards);
-            connection.getRemotePlayer().init(configuration, cards, playernames.length, 3);
-            System.out.println(connection.isOpen());
-        }
-        for (int i = playernames.length - 1; i > playernames.length - 1 - client_connections; i--) {
-            System.out.println(i - (playernames.length - client_connections));
-            //s2cconnections.get(i - (playernames.length - client_connections))
+            /*for (int i = playernames.length - 1; i > playernames.length - 1 - client_connections; i--) {
+                playernames[i] = s2cconnections.get(i - (playernames.length - client_connections)).getProjectName();
+                System.out.println(s2cconnections.get(i - (playernames.length - client_connections)).getProjectName());
+                System.out.println(i);
+                System.out.println(i - (playernames.length - client_connections));
+            } */
+            for (final S2CConnection connection : s2cconnections) {
+                connection.setPlayerNames(new ImmutableList<>(Arrays.asList(playernames)));
+                for (final String name : new ImmutableList<>(Arrays.asList(playernames)))
+                    System.out.println(name);
+                ImmutableList<Card> cards = new ImmutableList<>(configuration.getCards());
+                System.out.println(connection.getProjectName());
+                System.out.println(cards);
+                connection.getRemotePlayer().init(configuration, cards, playernames.length, 3);
+                System.out.println(connection.isOpen());
+            }
+            for (int i = playernames.length - 1; i > playernames.length - 1 - client_connections; i--) {
+                System.out.println(i - (playernames.length - client_connections));
+                //s2cconnections.get(i - (playernames.length - client_connections))
 
-            types[i] = PlayerType.REMOTE;
+                types[i] = PlayerType.REMOTE;
+            }
+            final GameBoardState gameBoardState = new GameBoardState(playernames, types, configuration, new ImmutableList<>(configuration.getCards()));
+            GameBoard GB = new GameBoard(configuration, gameBoardState);
+            setDefaultDesigns();
+            for (int i = 0; i < playernames.length; i++)
+                if(gameBoardState.getPlayers()[i].getPlayerType() == PlayerType.REMOTE)
+                    ((LocalRemotePlayer) gameBoardState.getPlayers()[i]).setS2CConnection(s2cconnections.get(i - (playernames.length - client_connections)));
+            gameBoardState.initGame(GB);
         }
-        final GameBoardState gameBoardState = new GameBoardState(playernames, types, configuration, new ImmutableList<>(configuration.getCards()));
-        GameBoard GB = new GameBoard(configuration, gameBoardState);
-        setDefaultDesigns();
-        for (int i = 0; i < playernames.length; i++)
-            if(gameBoardState.getPlayers()[i].getPlayerType() == PlayerType.REMOTE)
-                ((LocalRemotePlayer) gameBoardState.getPlayers()[i]).setS2CConnection(s2cconnections.get(i - (playernames.length - client_connections)));
-        gameBoardState.initGame(GB);
         System.out.println(configuration.getConfigDescription());
     }
     private static final void initClient(final GameConfigurationParser parser, final String projectname) throws IOException, RemoteException {
