7c67dd9be5dc53136085b6326134232f5595f13d
Florian Will <florian.will29@gmail.com>
2023-07-11 23:15:37 +0200

    Network handle Packets try

--- src/bauernhof/app/Start.java
+++ src/bauernhof/app/Start.java
@@ -24,7 +24,6 @@ import javax.swing.*;
 public class  Start {
     private static int client_connections = 1;
     private static int port = 1000;
-    private static int network = 2;
     public static void main(String args[]) throws Exception {
         // int network = Integer.valueOf(args[0]);
         // if (network >= 0)
@@ -104,7 +103,7 @@ public class  Start {
         System.out.println(configuration.getConfigDescription());
     }
     private static final void initClient(final GameConfigurationParser parser, final String projectname) throws IOException, RemoteException {
-        Socket socket = new Socket("localhost", port);
+        Socket socket = new Socket("127.0.0.1", port);
         ClientConnector myPLAYER = new ClientConnector(PlayerType.SIMPLE_AI, socket, parser, projectname);
         myPLAYER.handlePackets();
         return;
