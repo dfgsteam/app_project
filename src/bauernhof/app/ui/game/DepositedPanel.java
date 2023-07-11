21d779c43a1c12f4e1a132bd28a42839a23948a9
Kirill Pokhilenko <adeusamadeus@gmail.com>
2023-07-11 18:52:46 +0200

    forgot tjis one

--- src/bauernhof/app/ui/game/DepositedPanel.java
+++ src/bauernhof/app/ui/game/DepositedPanel.java
@@ -1,23 +1,27 @@
 package bauernhof.app.ui.game;
 
+import java.awt.Frame;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
 import javax.swing.JButton;
 
+import bauernhof.app.ui.game.listener.CardListener;
 import bauernhof.preset.card.Card;
 import bauernhof.preset.card.GCard;
 import sag.LayerPosition;
+import sag.SAGFrame;
 import sag.SAGPanel;
+import sag.elements.GElement;
 import sag.elements.GGroup;
 
 public class DepositedPanel extends SAGPanel implements ActionListener{
+    
+     private JButton Back;
+     private GameBoard gameBoard;
 
-    GameBoard gameBoard;
-    JButton Back;
-
-    public DepositedPanel(GameBoard gameBoard, ArrayList<Card> stack){
+    public DepositedPanel(GameBoard gameBoard, ArrayList<Card> list){
 
         this.gameBoard = gameBoard;
         
@@ -30,35 +34,38 @@ public class DepositedPanel extends SAGPanel implements ActionListener{
         this.setLayout(null);
         int x=100,y=120,i=0;
 
+        this.setVisible(true);
+
         GCard card;
 
-        for(; i< stack.size(); i++ ){
-            if(x+200 >= WIDTH){break;}
-            card = new GCard(stack.get(i));
+        for(; i< list.size(); i++ ){
+            if(x+200 >= this.VIEWPORT_WIDTH){break;}
+            card = new GCard(list.get(i));
             card.setMouseEventListener(gameBoard.getCardListener());
             top.addChild(card,x,y);
             x+=200;
             
         }
         x=100;y=-150;
-        for(; i< stack.size(); i++ ){
-            if(x+200 >= WIDTH){break;}
-            card = new GCard(stack.get(i));
+        for(; i< list.size(); i++ ){
+            if(x+200 >= this.VIEWPORT_WIDTH){break;}
+            card = new GCard(list.get(i));
             card.setMouseEventListener(gameBoard.getCardListener());
             cen.addChild(card,x,y);
             x+=200;
         
         }
         x=100;y=-350;
-        for(; i< stack.size(); i++ ){
-            card = new GCard(stack.get(i));
+        for(; i< list.size(); i++ ){
+            card = new GCard(list.get(i));
             card.setMouseEventListener(gameBoard.getCardListener());
             bot.addChild(card,x,y);
             x+=200;
 
         }
 
-        JButton Back = new JButton("Zuruck");
+        Back = new JButton("Zuruck");
+        Back.addActionListener(this::actionPerformed);
         Back.setBounds(this.VIEWPORT_WIDTH-250, this.VIEWPORT_HEIGHT-130, 150, 30);
         this.add(Back);
 
@@ -68,6 +75,7 @@ public class DepositedPanel extends SAGPanel implements ActionListener{
     public void actionPerformed(ActionEvent e) {
         if(e.getSource()==Back){
             gameBoard.getFrame().setSAGPanel(gameBoard.getMain());
+            
         }
     
 }
