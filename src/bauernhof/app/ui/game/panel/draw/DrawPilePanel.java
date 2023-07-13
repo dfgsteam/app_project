e20e48343b76507a99ee5addb482e249aa046e7e
julius.hunold_uni <info@julius-hunold.de>
2023-07-13 22:36:38 +0200

    add Listener -> button no function

--- src/bauernhof/app/ui/game/panel/draw/DrawPilePanel.java
+++ src/bauernhof/app/ui/game/panel/draw/DrawPilePanel.java
@@ -1,24 +1,16 @@
 package bauernhof.app.ui.game.panel.draw;
 
-import java.awt.Frame;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.ArrayList;
 import java.util.Stack;
 
 import javax.swing.JButton;
-
-import com.kitfox.svg.app.beans.SVGPanel;
-
 import bauernhof.app.ui.game.GameBoard;
-import bauernhof.app.ui.game.listener.BackButtonListener;
-import bauernhof.app.ui.game.listener.card.CardPopListener;
+import bauernhof.app.ui.game.listener.card.CardAddListener;
 import bauernhof.preset.card.Card;
 import bauernhof.preset.card.GCard;
 import sag.LayerPosition;
-import sag.SAGFrame;
 import sag.SAGPanel;
-import sag.elements.GElement;
 import sag.elements.GGroup;
 
 public class DrawPilePanel extends SAGPanel implements ActionListener{
@@ -49,7 +41,7 @@ public class DrawPilePanel extends SAGPanel implements ActionListener{
         for(; i< stack.size(); i++ ){
             if(x+200 >= this.VIEWPORT_WIDTH){break;}
             card = new GCard(stack.get(i));
-            card.setMouseEventListener(gameBoard.getCardListener());
+            card.setMouseEventListener(new CardAddListener(this.gameBoard, this.gameBoard.getPlayerId()));
             top.addChild(card,x,y);
             x+=200;
             
@@ -58,7 +50,7 @@ public class DrawPilePanel extends SAGPanel implements ActionListener{
         for(; i< stack.size(); i++ ){
             if(x+200 >= this.VIEWPORT_WIDTH){break;}
             card = new GCard(stack.get(i));
-            card.setMouseEventListener(gameBoard.getCardListener());
+            card.setMouseEventListener(new CardAddListener(this.gameBoard, this.gameBoard.getPlayerId()));
             cen.addChild(card,x,y);
             x+=200;
         
@@ -66,12 +58,14 @@ public class DrawPilePanel extends SAGPanel implements ActionListener{
         x=100;y=-350;
         for(; i< stack.size(); i++ ){
             card = new GCard(stack.get(i));
-            card.setMouseEventListener(gameBoard.getCardListener());
+            card.setMouseEventListener(new CardAddListener(this.gameBoard, this.gameBoard.getPlayerId()));
             bot.addChild(card,x,y);
             x+=200;
 
         }
 
+
+        // Muss neu -> Kein JButton
         Back = new JButton("Zuruck");
         Back.addActionListener(this::actionPerformed);
         Back.setBounds(this.VIEWPORT_WIDTH-250, this.VIEWPORT_HEIGHT-130, 150, 30);
@@ -87,7 +81,7 @@ public class DrawPilePanel extends SAGPanel implements ActionListener{
     @Override
     public void actionPerformed(ActionEvent e) {
         if(e.getSource()==Back){
-            gameBoard.getFrame().setSAGPanel(gameBoard.getMain());
+            //gameBoard.getFrame().setSAGPanel(gameBoard.getMain());
             
         }
     
