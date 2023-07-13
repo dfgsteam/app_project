ab26b736f28d4926a2795e640d9bd11037e55b5c
julius.hunold_uni <info@julius-hunold.de>
2023-07-13 22:19:45 +0200

    RoundPanel final

--- src/bauernhof/app/ui/game/panel/RoundPanal.java
+++ src/bauernhof/app/ui/game/panel/RoundPanal.java
@@ -3,40 +3,46 @@ package bauernhof.app.ui.game.panel;
 import java.awt.Color;
 
 import bauernhof.app.launcher.GameBoardState;
+import bauernhof.app.ui.game.GameBoard;
 import sag.LayerPosition;
-import sag.SAGPanel;
 import sag.elements.GGroup;
 import sag.elements.GText;
 import sag.elements.shapes.GRect;
 
 public class RoundPanal extends GGroup{
 
-    GText headline = new GText("R. 1");
+    GText roundCounter = new GText("1");
+
     GameBoardState gameBoardState;
 
-    public RoundPanal (SAGPanel mainPanel, GameBoardState gameBoardState) throws Exception {
+    public RoundPanal (GameBoard gameBoard) throws Exception {
         // Klassenvariablen erstellen
-        this.gameBoardState = gameBoardState;
+        this.gameBoardState = gameBoard.getGameBoardState();
 
         // Panel
-        GGroup panel = mainPanel.addLayer(LayerPosition.TOP_RIGHT);
+        GGroup panel = gameBoard.getMainPanel().addLayer(LayerPosition.CENTER);
 
         // Feld
-        GRect area = new GRect(0f, 0f, 80f, 50f, false, 0f, 0f);
+        GRect area = new GRect(0f, 0f, 110f, 90f, true, 0f, 0f);
         area.setFill(new Color(255, 255, 255, 255));
         area.setStroke(new Color(0, 0, 0), 3f);
-        panel.addChild(area, -90f, 10f);
+        panel.addChild(area, 0f, -10f);
 
-        // Zähler
-        this.headline.setAlignment(GText.TextAnchor.END);
-        this.headline.setFontSize(25f);
-        panel.addChild(this.headline, -21f, 45f);
-        System.out.println("ok");
+        // Überschrift
+        GText headline = new GText("Runde");
+        headline.setAlignment(GText.TextAnchor.MIDDLE);
+        headline.setFontSize(25f);
+        panel.addChild(headline, 0f, -15f);
+
+        // Rundenzähler
+        this.roundCounter.setAlignment(GText.TextAnchor.MIDDLE);
+        this.roundCounter.setFontSize(25f);
+        panel.addChild(this.roundCounter, 0f, 15f);
     }
 
     public void update() {
-        if (this.headline != null)
-            this.headline.setText("R. " + Integer.toString(this.gameBoardState.getRound()));
+        if (this.roundCounter != null)
+            this.roundCounter.setText(Integer.toString(this.gameBoardState.getRound()));
     }
 
 }
