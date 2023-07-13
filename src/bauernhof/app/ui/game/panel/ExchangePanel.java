7edec19cff66db99de0243448e693edee434e64e
julius.hunold_uni <info@julius-hunold.de>
2023-07-13 22:17:31 +0200

    Exchange final

--- src/bauernhof/app/ui/game/panel/ExchangePanel.java
+++ src/bauernhof/app/ui/game/panel/ExchangePanel.java
@@ -13,30 +13,28 @@ import sag.elements.GText;
 
 public class ExchangePanel extends SAGPanel{
 
-    private GameBoard gameBoard;
-    private GCard card;
     private SAGPanel panel;
 
     public ExchangePanel(GameBoard gameBoard){
-        this.gameBoard = gameBoard;
+        // Panel
         this.panel = new SAGPanel(GameBoard.WIDTH, GameBoard.HEIGTH);
 
-        GGroup Top = this.panel.addLayer(LayerPosition.TOP_CENTER);
-        Top.addChild(new GText("Welche Karte soll abgelegt werden?").setAlignment(GText.TextAnchor.MIDDLE), 0f,100f);
-
-        GGroup Mid = this.panel.addLayer(LayerPosition.CENTER_CENTER);
+        // Überschrift
+        GGroup headline = this.panel.addLayer(LayerPosition.TOP_CENTER);
+        headline.addChild(new GText("Welche Karte soll von dir abgelegt werden?").setAlignment(GText.TextAnchor.MIDDLE), 0f,100f);
         
-        ArrayList<Card> cards = this.gameBoard.getGameBoardState().getPlayers()[this.gameBoard.getPlayerId()].getCards();
+        // Ablagestapel
+        ArrayList<Card> cards = gameBoard.getGameBoardState().getPlayers()[gameBoard.getPlayerId()].getCards();
+        GGroup cardGroup = this.panel.addLayer(LayerPosition.CENTER_CENTER);
+        cardGroup.setScale(1.1f - (0.05f*cards.size()));
+        GCard card; 
         
+        int x = (int)(-210*((float)cards.size()/2)); // Bestimme Startposition
         
-        Mid.setScale(1.1f - (0.05f*cards.size()));
-        int x = (int)(-210*((float)cards.size()/2)), y = 0;
-        System.out.println(x);
-
-        for (int index=0; index < cards.size(); index++) {
+        for (int index=0; index < cards.size(); index++) { // Füge alle Karten aus der Hand hinzu
             card = new GCard(cards.get(index));
-            card.setMouseEventListener(new CardPopListener(this.gameBoard));
-            Mid.addChild(card,x, y);
+            card.setMouseEventListener(new CardPopListener(gameBoard));
+            cardGroup.addChild(card, x, 0);
             x+=210;
         }
     }
