73c6da46e33d19cfff65b89a1d28b2d48babeac6
Florian Will <florian.will29@gmail.com>
2023-07-14 19:13:46 +0200

    Deactivated broken getter methode in branche Dont commit broken lines pls

--- src/bauernhof/app/ui/game/panel/deposited/DepositedPanel.java
+++ src/bauernhof/app/ui/game/panel/deposited/DepositedPanel.java
@@ -1,33 +1,28 @@
 package bauernhof.app.ui.game.panel.deposited;
 
-import java.awt.Frame;
+import bauernhof.app.ui.game.GameBoard;
+import bauernhof.app.ui.game.listener.card.CardAddListener;
+import bauernhof.preset.card.Card;
+import bauernhof.preset.card.GCard;
+import sag.LayerPosition;
+import sag.SAGPanel;
+import sag.elements.GGroup;
+
+import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
-import javax.swing.JButton;
+public class DepositedPanel extends SAGPanel implements ActionListener {
 
-import bauernhof.app.ui.game.GameBoard;
-import bauernhof.app.ui.game.listener.card.CardAddListener;
-import bauernhof.app.ui.game.listener.card.CardPopListener;
-import bauernhof.preset.card.Card;
-import bauernhof.preset.card.GCard;
-import sag.LayerPosition;
-import sag.SAGFrame;
-import sag.SAGPanel;
-import sag.elements.GElement;
-import sag.elements.GGroup;
+    private JButton Back;
+    private GameBoard gameBoard;
 
-public class DepositedPanel extends SAGPanel implements ActionListener{
-    
-     private JButton Back;
-     private GameBoard gameBoard;
-
-    public DepositedPanel(GameBoard gameBoard){
+    public DepositedPanel(GameBoard gameBoard) {
 
         this.gameBoard = gameBoard;
         ArrayList<Card> list = this.gameBoard.getGameBoardState().getDepositedCards();
-        
+
         GGroup top = this.addLayer(LayerPosition.TOP_LEFT);
         top.setScale(0.75f);
         GGroup cen = this.addLayer(LayerPosition.CENTER_LEFT);
@@ -35,51 +30,54 @@ public class DepositedPanel extends SAGPanel implements ActionListener{
         GGroup bot = this.addLayer(LayerPosition.BOTTOM_LEFT);
         bot.setScale(0.75f);
         this.setLayout(null);
-        int x=100,y=120,i=0;
+        int x = 100, y = 120, i = 0;
 
         this.setVisible(true);
 
         GCard card;
 
-        for(; i< list.size(); i++ ){
-            if(x+200 >= this.VIEWPORT_WIDTH){break;}
+        for (; i < list.size(); i++) {
+            if (x + 200 >= this.VIEWPORT_WIDTH) {
+                break;
+            }
             card = new GCard(list.get(i));
             card.setMouseEventListener(new CardAddListener(this.gameBoard, this.gameBoard.getPlayerId()));
-            top.addChild(card,x,y);
-            x+=200;
-            
+            top.addChild(card, x, y);
+            x += 200;
+
         }
-        x=100;y=-150;
-        for(; i< list.size(); i++ ){
-            if(x+200 >= this.VIEWPORT_WIDTH){break;}
+        x = 100;
+        y = -150;
+        for (; i < list.size(); i++) {
+            if (x + 200 >= this.VIEWPORT_WIDTH) {
+                break;
+            }
             card = new GCard(list.get(i));
             card.setMouseEventListener(new CardAddListener(this.gameBoard, this.gameBoard.getPlayerId()));
-            cen.addChild(card,x,y);
-            x+=200;
-        
+            cen.addChild(card, x, y);
+            x += 200;
+
         }
-        x=100;y=-350;
-        for(; i< list.size(); i++ ){
+        x = 100;
+        y = -350;
+        for (; i < list.size(); i++) {
             card = new GCard(list.get(i));
             card.setMouseEventListener(new CardAddListener(this.gameBoard, this.gameBoard.getPlayerId()));
-            bot.addChild(card,x,y);
-            x+=200;
+            bot.addChild(card, x, y);
+            x += 200;
 
         }
 
         Back = new JButton("Zuruck");
         Back.addActionListener(this::actionPerformed);
-        Back.setBounds(this.VIEWPORT_WIDTH-250, this.VIEWPORT_HEIGHT-130, 150, 30);
+        Back.setBounds(this.VIEWPORT_WIDTH - 250, this.VIEWPORT_HEIGHT - 130, 150, 30);
         this.add(Back);
-
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        if(e.getSource()==Back){
-            gameBoard.getFrame().setSAGPanel(gameBoard.getMain());
-            
+        if (e.getSource() == Back) {
+            //gameBoard.getFrame().setSAGPanel(gameBoard.getMain());
         }
-    
-}
+    }
 }
