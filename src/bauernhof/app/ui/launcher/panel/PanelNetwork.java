9de37fd969d8d647526f6e69d51b603369629990
julius.hunold_uni <info@julius-hunold.de>
2023-07-15 19:45:37 +0200

    network input start

--- src/bauernhof/app/ui/launcher/panel/PanelNetwork.java
+++ src/bauernhof/app/ui/launcher/panel/PanelNetwork.java
@@ -3,15 +3,28 @@ package bauernhof.app.ui.launcher.panel;
 import javax.swing.*;
 
 import bauernhof.app.ui.launcher.UiLauncher;
+import bauernhof.preset.PlayerType;
 
 import java.awt.*;
 import java.awt.event.*;
 import java.io.IOException;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 public class PanelNetwork {
     private UiLauncher uiLauncher;
     private JPanel panel;
     private int status;
+
+    final private String[] examPlayerNames = {"Florian", "Cemil", "Viktor", "Kirill"};
+    final private String[] playerTypeName = {"Computer Einfach", "Computer Mittel", "Computer Schwer", "Lokaler Spieler", "Remote"};
+    final private PlayerType[] playerTypeType = {PlayerType.RANDOM_AI, PlayerType.SIMPLE_AI, PlayerType.ADVANCED_AI, PlayerType.HUMAN, PlayerType.REMOTE};
+    final private String[] playerColorName = {"Rot", "Magenta", "Blau", "Orange", "Cyan", "Pink"};
+    final private Color[] playerColorType = {Color.RED, Color.MAGENTA, Color.BLUE, Color.ORANGE, Color.CYAN, Color.PINK};
+
+    private JComboBox<String> playerTypeCombo[] = new JComboBox[4];
+    private JTextField playerNameField[] = new JTextField[4];
+    private JComboBox<String> playerColorCombo[] = new JComboBox[4];
 
     private JButton buttonCreateServer, buttonJoinServer, buttonStartServer;
 
@@ -58,6 +71,7 @@ public class PanelNetwork {
         this.status = 1;
         this.createNewPanal("multiGameServer");
         this.addButtonStartGame();
+        this.createServerView();
         this.uiLauncher.setPanelNetworkPlayer();
     }
 
@@ -181,5 +195,92 @@ public class PanelNetwork {
 
     public JPanel getPanel() {
         return this.panel;
+    }
+
+    public void createServerView() {
+        InetAddress IP = null;
+        try {
+            IP = InetAddress.getLocalHost();
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+
+        JLabel serverAdress= new JLabel(IP.getHostAddress() + ":8000");
+        serverAdress.setForeground(Color.white);
+        serverAdress.setFont(new Font("Arial", Font.PLAIN, 30));
+        serverAdress.setBounds(200, 150, 300, 45); 
+        this.panel.add(serverAdress);
+
+        int xPos = 218; // xPos wird pro Player statisch verändert. Die yPos wird anhand deinzelnen Objektlängen + 5px/15px Abstand berechnet
+        int yPos = 50;
+        // Erstelle für jeden Spieler inputs
+        for (int index=0; index < 4; index ++) { // playerId = 0, 1, 2, 3
+            
+            // Spielertyp //
+
+            // -> JLabel
+            JLabel addPlayerTypeLabel = new JLabel("Spielertyp:");
+            addPlayerTypeLabel.setForeground(Color.white);
+            addPlayerTypeLabel.setBounds(yPos, xPos, 180, 45); 
+            this.panel.add(addPlayerTypeLabel);
+            
+            // -> JComboBox - Dropdown
+            JComboBox<String> addPlayerTypeCombo = new JComboBox<>(this.playerTypeName);
+            addPlayerTypeCombo.setBounds(yPos, xPos+40, 180, 45);
+            if (index > 1) {
+                addPlayerTypeCombo.addItem("Keiner");
+                //addPlayerTypeCombo.addItemListener(new ListenerLocalAddUser(this, index));
+            }
+            this.panel.add(addPlayerTypeCombo);
+            this.playerTypeCombo[index] = addPlayerTypeCombo;
+
+            yPos += 200;
+
+            // Spielernamen //
+
+            // -> Label
+            JLabel addPlayerNameLabel = new JLabel("Name:");
+            addPlayerNameLabel.setForeground(Color.white);
+            addPlayerNameLabel.setBounds(yPos, xPos, 180, 45);
+            this.panel.add(addPlayerNameLabel);
+
+            // -> JComboBox - Dropdown
+            JTextField addPlayerNameField = new JTextField(this.examPlayerNames[index]);
+            addPlayerNameField.setBounds(yPos, xPos+40, 180, 45);
+            this.panel.add(addPlayerNameField);
+            this.playerNameField[index] = addPlayerNameField;
+
+            yPos += 200;
+
+            // Spielerfarbe
+
+            // -> Label
+            JLabel addPlayerColoLabel = new JLabel("Farbe:");
+            addPlayerColoLabel.setForeground(Color.white);
+            addPlayerColoLabel.setBounds(yPos, xPos, 180, 45);
+            this.panel.add(addPlayerColoLabel);
+
+            // -> JComboBox - Dropdown
+            JComboBox<String> addPlayerColorCombo = new JComboBox<>(this.playerColorName);
+            addPlayerColorCombo.setSelectedIndex(index);
+            addPlayerColorCombo.setBounds(yPos, xPos+40, 180, 45);
+            this.panel.add(addPlayerColorCombo);
+            this.playerColorCombo[index] = addPlayerColorCombo;
+
+            yPos += 200;
+
+            // Delete Button //
+            
+            if (index > 1) { // 2 Spieler sind verpflichtend -> Spieler 1, 2 kann man löschen
+                JButton addPlayerDelButton = new JButton("Löschen");
+                addPlayerDelButton.setBounds(yPos, xPos+17, 100, 68);
+                addPlayerDelButton.addActionListener(null);
+                this.panel.add(addPlayerDelButton);
+            }
+
+            // Nächste Zeile
+            yPos = 50;
+            xPos += 112;
+        }  
     }
 }
