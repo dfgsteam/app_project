2897223b9ce465842b75ff0a8b121a98b5ca2127
Julius Hunold <hunoldjulius@gmail.com>
2023-07-19 01:21:08 +0200

    komischer commit :/

--- src/bauernhof/app/initLauncher.java
+++ src/bauernhof/app/initLauncher.java
@@ -3,11 +3,11 @@ package bauernhof.app;
 import java.io.File;
 import java.io.IOException;
 
-import LauncherSettings;
+import bauernhof.app.launcherSettings.*;
 import bauernhof.app.ui.launcher.UiLauncher;
 
 public class InitLauncher {
-    public static void main(String args[]) throws IOException, InterruptedException, SeEx {
-        new UiLauncher(new LauncherSettings().parse(new File("gameconfigs/game/general.xml")));
+    public InitLauncher() throws IOException, InterruptedException, LauncherSettingsException {
+        new UiLauncher(new LauncherSettingsParser().parse(new File("gameconfigs/game/general.xml")));
     }
 }
