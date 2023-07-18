524e6ff74ba95ca29c9c14ff7f3f82b557ee101f
julius.hunold_uni <info@julius-hunold.de>
2023-07-18 21:18:59 +0200

    planelExchange new

--- src/bauernhof/app/ui/game/panel/PanelExchangeCards.java
+++ src/bauernhof/app/ui/game/panel/PanelExchangeCards.java
@@ -3,6 +3,7 @@ package bauernhof.app.ui.game.panel;
 import java.util.ArrayList;
 
 import bauernhof.app.ui.game.UiGame;
+import bauernhof.app.ui.game.listener.ListenerBackButton;
 import bauernhof.app.ui.game.listener.card.CardPopListener;
 import bauernhof.preset.card.Card;
 import bauernhof.preset.card.GCard;
@@ -10,6 +11,7 @@ import sag.LayerPosition;
 import sag.SAGPanel;
 import sag.elements.GGroup;
 import sag.elements.GText;
+import sag.elements.shapes.GRect;
 
 /**
  * This class represents a panel for exchanging cards in the game UI.
@@ -22,6 +24,7 @@ import sag.elements.GText;
 public class PanelExchangeCards extends SAGPanel {
 
     private SAGPanel panel;
+    private UiGame uiGame;
 
     /**
      * Constructs a new PanelExchangeCards object.
@@ -29,18 +32,28 @@ public class PanelExchangeCards extends SAGPanel {
      * @param UiGame The UiGame object that represents the game UI.
      */
     public PanelExchangeCards(UiGame UiGame) {
-        this.panel = new SAGPanel(UiGame.WIDTH, UiGame.HEIGTH);
+        this.uiGame = uiGame;
+
+        this.panel = new SAGPanel(bauernhof.app.ui.game.UiGame.WIDTH, bauernhof.app.ui.game.UiGame.HEIGTH);
+        GGroup headlineGroup = this.panel.addLayer(LayerPosition.TOP_CENTER);
+        GGroup cardGroup = this.panel.addLayer(LayerPosition.CENTER_CENTER);
+
 
         // Überschrift
-        GGroup headline = this.panel.addLayer(LayerPosition.TOP_CENTER);
-        headline.addChild(new GText("Welche Karte soll von dir abgelegt werden?").setAlignment(GText.TextAnchor.MIDDLE), 0f, 100f);
+        GText headlineHeadline = new GText("Welche Karte soll von dir abgelegt werden?");
+        headlineHeadline.setAlignment(GText.TextAnchor.MIDDLE);
+        headlineHeadline.setFontSize(40f);
+        headlineHeadline.setBold(true);
+        headlineGroup.addChild(headlineHeadline, 0f, 100f);
 
         // Ablagestapel
-        ArrayList<Card> cards = UiGame.getGameBoardState().getPlayers()[UiGame.getPlayerId()].getCards();
-        GGroup cardGroup = this.panel.addLayer(LayerPosition.CENTER_CENTER);
-        cardGroup.setScale(1.1f - (0.05f * cards.size()));
-        GCard card;
+        //cardGroup.setScale(1.1f - (0.05f * this.uiGam)); //-> aus Settings hand pro player+1
 
+        
+    }
+
+
+    public void update() {
         int x = (int) (-210 * ((float) cards.size() / 2)); // Bestimme Startposition
 
         for (int index = 0; index < cards.size(); index++) { // Füge alle Karten aus der Hand hinzu
