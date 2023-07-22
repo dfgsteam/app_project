6976d15b741fa308f44c664d2259e00a8257d801
Viktor Tevosyan <viktor.tevosyan@c146.cip.loc>
2023-07-22 18:27:08 +0200

    Random_AI and Simple_AI completely done

--- src/bauernhof/app/player/types/Random_AI.java
+++ src/bauernhof/app/player/types/Random_AI.java
@@ -13,7 +13,13 @@ import java.util.Random;
  * @author Viktor Tevosyan
  * @date 26.06.2023
  * This class represents a Random_AI, which extends from AbstractGamePlayer, because it is a GamePlayer.
- * It implements the AIHeader interface, in order to follow the structure of an AI class.
+ * It implements the AIHeader interface, in order to follow the structure of an Artifical Intelligence, which is written in the interface.
+ * The Random_AI has the role of a very bad player, who has no idea of the game it plays and makes random moves:
+ * - Does not understand, how the board looks like
+ * - Does not understand the moves
+ * - Does not consider the players
+ * - Can't determine, which move is good or bad
+ * - Plays fortune game
  */
 public class Random_AI extends AbstractGamePlayer implements AIHeader{
 
@@ -37,18 +43,15 @@ public class Random_AI extends AbstractGamePlayer implements AIHeader{
             else to_take = cardFromDeposit();       
         }
 
-        else {
-            to_take = cardFromDeposit();            //else we know, that the deposited area is not empty --> take from there
-        }
+        else { to_take = cardFromDeposit(); }        //else we know, that the deposited area is not empty --> take from there
+            
+        
 
         //----------------------------------
-        if (Math.random() < 1.0/ (configuration.getNumCardsPerPlayerHand()+1)) {
-            to_deposit = to_take;                   //by the probability of 1/owncards+1 -> deposited card is the taken card
-        }
+        if (Math.random() < 1.0/ (configuration.getNumCardsPerPlayerHand()+1)) { to_deposit = to_take;  }             //by the probability of 1/owncards+1 -> deposited card is the taken card
+            
         
-        else {
-            to_deposit = removeFromOwn(to_take);       //else a card from own
-        }
+        else { to_deposit = removeFromOwn(to_take); }     //else a card from own
 
         return new Move(to_take, to_deposit);       //return calculated move
     }
@@ -73,12 +76,18 @@ public class Random_AI extends AbstractGamePlayer implements AIHeader{
     }
 
     @Override
+    /**
+     * @return Random card from Deposit
+     */
     public Card cardFromDeposit() {
        int deposit_random_index = depositRandomNumber();        //generate the number of the card in the deposit area
        return gameBoard.getDepositedCards().get(deposit_random_index);      //return the card by its number of the deposited area
     }
 
     @Override
+    /**
+     * @return Card from the DrawPileStack
+     */
     public Card cardFromStack() {
         if (gameBoard.getDrawPileCards().isEmpty())     //just return no reference, if the drawpile is empty
             return null;
@@ -86,6 +95,9 @@ public class Random_AI extends AbstractGamePlayer implements AIHeader{
     }
 
     @Override
+    /**
+     * @return Random Card from own Hand
+     */
     public Card removeFromOwn(Card to_take) {
         int own_random_index = ownCardNumber();     //generate the number of own card
         return playercards.getCards().get(own_random_index);        //return the card by its number
