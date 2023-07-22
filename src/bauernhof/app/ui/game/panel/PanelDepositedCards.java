14b6bc9111603ed989bfe30021255426a8e0e463
PaintableToast <rckimyon@yahoo.com>
2023-07-22 18:20:33 +0200

    Kirill Deposited Merge

--- src/bauernhof/app/ui/game/panel/PanelDepositedCards.java
+++ src/bauernhof/app/ui/game/panel/PanelDepositedCards.java
@@ -3,6 +3,8 @@ package bauernhof.app.ui.game.panel;
 import bauernhof.app.card.Ca;
 import bauernhof.app.ui.game.UiGame;
 import bauernhof.app.ui.game.listener.ListenerBackButton;
+import bauernhof.app.ui.game.listener.ListenerDeposited;
+import bauernhof.app.ui.game.listener.card.ListenerCard;
 import bauernhof.app.ui.game.listener.card.ListenerCardAdd;
 import bauernhof.preset.card.GCard;
 import sag.ChildNotFoundException;
@@ -32,7 +34,7 @@ public class PanelDepositedCards extends SAGPanel {
         GGroup buttonBackPanel = this.panel.addLayer(LayerPosition.BOTTOM_CENTER);
 
         // Überschrift
-        GText headlineHeadline = new GText("Nachziehstapel");
+        GText headlineHeadline = new GText("Ablagestapel");
         headlineHeadline.setAlignment(GText.TextAnchor.MIDDLE);
         headlineHeadline.setFontSize(40f);
         headlineHeadline.setBold(true);
@@ -54,7 +56,7 @@ public class PanelDepositedCards extends SAGPanel {
         buttonBackBG.setMouseEventListener(new ListenerBackButton(this.uiGame, buttonBackHeadline, 2));
     }
 
-    public void update() {
+    public void update() throws ChildNotFoundException {
 
         // Bitte schreibe die Kartenerstellung in die update()-Methode. So müssen wir nicht mehr das Panel komplett neuladen.
 
@@ -63,35 +65,52 @@ public class PanelDepositedCards extends SAGPanel {
         // G-Cards bekommst du mit ((Ca) this.uiGame.getGameBoardState().getDepositedCards().get(index)).getGCard()
         
 
-        int size = this.uiGame.getGameBoardState().getDepositedCards().size();
+        int size = this.uiGame.getGame().getDepositedCards().size();
         groupCards = this.panel.addLayer(LayerPosition.CENTER_CENTER);
-        groupCards.setScale(0.68f);
         this.setLayout(null);
         this.setVisible(true);
-        GCard card;
-        card = ((Ca) this.uiGame.getGameBoardState().getDepositedCards().get(0)).getGCard();
-        card.setMouseEventListener(new CardListener());
-        int x = (int)(-210*((float)this.uiGame.getGameBoardState().getDepositedCards().size()/8)),y=110,i=1;
+        GCard card = null;
+        if (size <= uiGame.getGame().getConfiguration().getNumDepositionAreaSlots() / 2) {
+            groupCards.setScale(1.25F);
+            for (int i = 0; i < size; i++) {
+                card = ((Ca) uiGame.getGame().getDepositedCards().get(i)).getGCard();
+                groupCards.addChild(card, (size % 2 == 0 ? 105 : 0) + 210 * (-(size / 2) + i), 0);
+            }
+        } else {
+            groupCards.setScale(1.0F);
+            for (int i = 0; i < size; i++) {
+                card = ((Ca) uiGame.getGame().getDepositedCards().get(i)).getGCard();
+                groupCards.addChild(card, (size % 2 == 0 ? i % 2 == 0 ? 110 : 0 : 0 ) + 110*(-(size/2) + i), i % 2 == 0 ? -150 : 150); // Vllt nicht soviel elvis
+            }
+        }
+        if(card != null) card.setMouseEventListener(new ListenerCard());
+        /*card = ((Ca) this.uiGame.getGame().getDepositedCards().get(0)).getGCard();
+        card.setMouseEventListener(new ListenerCard());
+
+        int x = (int)(-210*((float)this.uiGame.getGame().getDepositedCards().size()/8)),y=110,i=1;
 
         groupCards.addChild(card, 0, -200);
 
         for(; i< size; i++ ){
-            if(x+400 >= this.panel.VIEWPORT_WIDTH){break;}
-            card =((Ca) this.uiGame.getGameBoardState().getDepositedCards().get(i)).getGCard();
-            card.setMouseEventListener(new CardListener());
+            if(x+400 >= this.panel.VIEWPORT_WIDTH){
+                break;
+            }
+            card =((Ca) this.uiGame.getGame().getDepositedCards().get(i)).getGCard();
+            card.setMouseEventListener(new ListenerCard());
             groupCards.addChild(card,x,y);
             x+=210;
 
         }
-        x = (int)(-210*((float)this.uiGame.getGameBoardState().getDepositedCards().size()/8));y=-3450;
+        x = (int)(-210*((float)this.uiGame.getGame().getDepositedCards().size()/8));
+        y=-3450;
         for(; i< size; i++ ){
             if(x+400 >= this.panel.VIEWPORT_WIDTH){break;}
-            card =((Ca) this.uiGame.getGameBoardState().getDepositedCards().get(i)).getGCard();
-            card.setMouseEventListener(new CardListener());
+            card =((Ca) this.uiGame.getGame().getDepositedCards().get(i)).getGCard();
+            card.setMouseEventListener(new ListenerCard());
             groupCards.addChild(card,x,y);
             x+=210;
 
-        }
+        }*/
     }
 
     public void clear() throws ChildNotFoundException {
