aad89667038dea628c7f384fcfdbd0039ef73a73
Florian Will <florian.will01@stud.uni-goettingen.de>
2023-07-24 10:40:44 +0200

    javadoc 6

--- src/bauernhof/app/player/types/HumanPlayer.java
+++ src/bauernhof/app/player/types/HumanPlayer.java
@@ -3,20 +3,52 @@ package bauernhof.app.player.types;
 import bauernhof.app.player.PlayerCards;
 import bauernhof.app.player.AbstractGamePlayer;
 import bauernhof.app.system.GameBoard;
+import bauernhof.app.ui.game.UiGame;
 import bauernhof.preset.Move;
 import bauernhof.preset.Settings;
 
 /**
- * Human Player - PlayerType Human Instanz
+ * This class represents a human player in the Hofbauern game.
+ * It extends the AbstractGamePlayer class and implements the request() method
+ * to allow human players to make moves during the game.
  *
+ * The HumanPlayer class holds references to the game settings, player cards,
+ * and the game board system, enabling human players to interact with the game state.
+ * The class uses the UiGame object from the GameBoard class to request moves from the human player
+ * through the graphical user interface.
+ *
+ * The HumanPlayer class is a concrete implementation of a player and provides the necessary methods
+ * to participate in the game and make valid moves.
+ * 
+ * @see AbstractGamePlayer
+ * @see GameBoard
+ * @see Move
+ * @see Settings
+ * @see PlayerCards
+ * @see UiGame
  * @author Ramon Cemil Kimyon
- * @date 10.06.2023 15:15
  */
 public class HumanPlayer extends AbstractGamePlayer {
+        
+    /**
+     * Constructs a new HumanPlayer object with the given game settings, player cards, and game board system.
+     *
+     * @param settings     The Settings object representing game settings.
+     * @param playercards  The PlayerCards object representing the player's hand.
+     * @param gamesystem   The GameBoard object representing the game board system.
+     */
+
     public HumanPlayer(final Settings settings, final PlayerCards playercards, final GameBoard gamesystem) {
         super(settings, playercards, gamesystem);
     }
 
+    /**
+     * Requests a move from the human player during their turn.
+     *
+     * @return The Move object representing the move made by the human player.
+     * @throws Exception If an error occurs during the move request.
+     */
+    
     @Override
     public Move request() throws Exception {
         return GameBoard.getGraphics().requestMoveFromCurrentHumanPlayer();
--- src/bauernhof/app/system/Game.java
+++ src/bauernhof/app/system/Game.java
@@ -10,22 +10,145 @@ import bauernhof.preset.card.Card;
 import java.util.ArrayList;
 
 /**
+ * The Game interface defines the contract for managing the state and actions of a game. It provides methods to access
+ * various game-related information, such as player IDs, scores, cards, rounds, and game settings. The Game interface
+ * also supports executing moves in the game and determining the winner.
+ *
+ * Implementing classes that represent specific games should implement this interface to provide the necessary game logic
+ * and functionality. The interface facilitates a common set of methods for interacting with different game types.
+ *
+ * The Game interface assumes the existence of a player and card system that allows players to make moves and manage their
+ * cards during the game. Players have access to their cards and can execute moves based on the game's rules and mechanics.
+ *
+ * @see PlayerCards
+ * @see GameConfiguration
+ * @see ImmutableList
+ * @see Move
+ * @see Settings
+ * @see Card
+ * @see java.util.ArrayList
+ * @see java.util.List
+ * @see java.util.Collections
  * @author Ramon Cemil Kimyon
  */
 public interface Game {
+      
+    /**
+     * Gets the ID of the current player who has the turn to make a move.
+     *
+     * @return The ID of the current player.
+     */
+
     int getCurrentPlayerID();
+
+    /**
+     * Gets an immutable list containing the scores of all players in the game.
+     *
+     * @return An ImmutableList of Integer values representing the scores of all players.
+     */
+
     ImmutableList<Integer> getAllScores();
+
+    /**
+     * Gets the list of cards that have been deposited during the game.
+     *
+     * @return An ArrayList of Card objects representing the deposited cards in the game.
+     */
+
     ArrayList<Card> getDepositedCards();
+
+    /**
+     * Gets an immutable list containing the cards remaining in the draw pile.
+     *
+     * @return An ImmutableList of Card objects representing the cards remaining in the draw pile.
+     */
+
     ImmutableList<Card> getDrawPileCards();
+
+    /**
+     * Gets the cards held by a specific player.
+     *
+     * @param playerid The ID of the player whose cards are to be retrieved.
+     * @return A PlayerCards object representing the cards held by the specified player.
+     */
+
     PlayerCards getPlayerCards(final int playerid);
+
+    /**
+     * Gets the cards held by the current player who has the turn to make a move.
+     *
+     * @return A PlayerCards object representing the cards held by the current player.
+     */
+
     PlayerCards getCurrentPlayerCards();
+
+    /**
+     * Gets the current round number of the game.
+     *
+     * @return The current round number.
+     */
+
     int getRound();
+
+    /**
+     * Executes a move in the game based on the provided Move object.
+     *
+     * @param move The Move object representing the move to be executed.
+     * @return True if the move is successfully executed, false otherwise.
+     * @throws Exception If an error occurs while executing the move.
+     */
+
     boolean executeMove(final Move move) throws Exception;
+
+    /**
+     * Gets the total number of players participating in the game.
+     *
+     * @return The total number of players.
+     */
+
     int getNumPlayers();
+
+    /**
+     * Gets the ID of the winner of the game.
+     *
+     * @return The ID of the player who is the winner, or the total number of players if there is no winner yet.
+     */
+
     int getWinnerID();
 
+    /**
+     * Gets the configuration settings of the game.
+     *
+     * @return A GameConfiguration object representing the game settings and rules.
+     */
+
     GameConfiguration getConfiguration();
+
+    /**
+     * Gets the name of a player based on their player ID.
+     *
+     * @param playerid The ID of the player whose name is to be retrieved.
+     * @return The name of the player with the specified ID.
+     * @throws Exception If the player ID is invalid or an error occurs while retrieving the name.
+     */
+
     String getName(final int playerid) throws Exception;
+
+    /**
+     * Gets the score of a player based on their player ID.
+     *
+     * @param playerid The ID of the player whose score is to be retrieved.
+     * @return The score of the player with the specified ID.
+     * @throws Exception If the player ID is invalid or an error occurs while retrieving the score.
+     */
+
     int getScore(final int playerid) throws Exception;
+
+    /**
+     * Gets the settings of the game.
+     *
+     * @return A Settings object representing the game settings and configurations.
+     */
+    
     Settings getSettings();
 }
