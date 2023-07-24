4e2975acc8050e976a5f7020df7839e5c7bbf251
Florian Will <florian.will01@stud.uni-goettingen.de>
2023-07-24 10:47:13 +0200

    javadoc 10

--- src/bauernhof/app/system/Tournament.java
+++ src/bauernhof/app/system/Tournament.java
@@ -3,9 +3,24 @@ package bauernhof.app.system;
 import bauernhof.preset.*;
 
 import java.util.ArrayList;
+
 /**
+ * The Tournament class represents a tournament of games played between different player types. It manages the tournament
+ * rounds, tracks the number of wins for each player type, and displays the results at the end of the tournament.
+ * The Tournament class uses the GameSystem to simulate individual games and keeps track of the overall results.
+ *
+ * The Tournament class allows setting up and running a tournament with the specified settings and game configuration.
+ * It uses the GameSystem to create and manage game instances for each tournament round. The results of each round are
+ * accumulated to determine the overall winners and the number of wins for each player type.
+ *
+ * @see GameSystem
+ * @see Settings
+ * @see GameConfiguration
+ * @see java.util.ArrayList
+ * @see java.util.List
  * @author Florian Will
  */
+
 public class Tournament {
     private ArrayList<Integer> wins = new ArrayList<>();
     private Settings settings;
@@ -13,6 +28,15 @@ public class Tournament {
     private int counter = 0;
     private
     final ArrayList<GameSystem> states = new ArrayList<>();
+
+    /**
+     * Constructs a new Tournament instance with the specified settings and game configuration.
+     *
+     * @param settings      The settings for the tournament, including player types, names, and number of rounds.
+     * @param configuration The configuration for the games in the tournament, including card types and rules.
+     * @throws Exception If an error occurs while initializing the tournament.
+     */
+
     public Tournament(final Settings settings, final GameConfiguration configuration) throws Exception {
         this.settings = settings;
         this.configuration = configuration;
@@ -20,6 +44,13 @@ public class Tournament {
             wins.add(0);
     }
 
+    /**
+     * Initializes and runs the tournament. It simulates multiple rounds of games and tracks the results of each round.
+     * At the end of the tournament, it displays the overall results and the number of wins for each player type.
+     *
+     * @throws Exception If an error occurs during the tournament or game simulations.
+     */
+    
     public final void initTournament() throws Exception {
         GameSystem system;
         settings.showGUI = false;
