e003a1cfdca75208c4f6614ea16069641f98f5e7
Florian Will <florian.will01@stud.uni-goettingen.de>
2023-07-24 11:15:43 +0200

    javadoc 111

--- src/bauernhof/app/exceptions/WrongInputException.java
+++ src/bauernhof/app/exceptions/WrongInputException.java
@@ -1,6 +1,17 @@
 package bauernhof.app.exceptions;
 
+/**
+ * Exception class for indicating that the input provided is incorrect or invalid.
+ * This exception is thrown when there is a problem with the user input.
+ * @author Viktor Tevosyan
+ */
+
 public class WrongInputException extends Exception {
+
+    /**
+     * Constructs a new WrongInputException with a default error message.
+     */
+
     public WrongInputException() {
         super("Wrong input");
     }
--- src/bauernhof/app/networking/Client.java
+++ src/bauernhof/app/networking/Client.java
@@ -17,14 +17,46 @@ import java.io.IOException;
 import java.net.Socket;
 import java.util.ArrayList;
 
+/**
+ * The Client class represents a client-side connection to a remote game server
+ * in a networked game of "Bauernhof".
+ * This class extends the C2SConnection class, which handles the client-side
+ * communication with the server.
+ * The Client class is responsible for initializing and managing the player's game state
+ * and handling the interactions with the server.
+ * @author Florian Will
+ */
+
 public class Client extends C2SConnection {
     private Settings settings;
     private AbstractGamePlayer player;
+
+    /**
+     * Constructs a new Client object with the given settings, socket, parser, and project name.
+     *
+     * @param settings      The game settings.
+     * @param socket        The socket for the client-server connection.
+     * @param parser        The game configuration parser.
+     * @param projectName   The name of the project or game.
+     * @throws IOException       If an I/O error occurs while establishing the connection.
+     * @throws RemoteException   If a remote exception occurs during the communication with the server.
+     */
+
     public Client(final Settings settings, final Socket socket, final GameConfigurationParser parser, final String porjectname) throws IOException, RemoteException {
         super(socket, parser, porjectname);
         this.settings = settings;
         handlePackets();
     }
+
+    /**
+     * Handles the game initialization when receiving the game configuration from the server.
+     *
+     * @param gameConfiguration The game configuration received from the server.
+     * @param initialDrawPile   The initial draw pile of cards for the game.
+     * @param playerNames       The names of all players in the game.
+     * @param playerid          The ID of the current player.
+     * @throws Exception        If an error occurs during game initialization.
+     */
 
     @Override
     protected void onInit(GameConfiguration game_configuration, ImmutableList<Card> initialDrawPile, ImmutableList<String> playerNames, int playerid) throws Exception {
@@ -53,6 +85,13 @@ public class Client extends C2SConnection {
         GameBoard.graphics = new UiGame(game);
     }
 
+    /**
+     * Handles the player's move request from the server.
+     *
+     * @return The move selected by the player.
+     * @throws Exception If an error occurs during move selection.
+     */
+
     @Override
     protected Move onRequest() throws Exception {
         final Move move = player.request();
@@ -61,22 +100,50 @@ public class Client extends C2SConnection {
         return move;
     }
 
+    /**
+     * Handles the update received from the server after an opponent's move.
+     *
+     * @param move The opponent's move.
+     * @throws Exception If an error occurs during the update process.
+     */
+
     @Override
     protected void onUpdate(Move move) throws Exception {
         this.player.update(move);
         GameBoard.graphics.update(false);
     }
 
+    /**
+     * Retrieves the player's current score from the game.
+     *
+     * @return The player's current score.
+     * @throws Exception If an error occurs while fetching the score.
+     */
+
     @Override
     protected int onGetScore() throws Exception {
         return player.getScore();
     }
+
+    /**
+     * Retrieves the player's name from the game.
+     *
+     * @return The player's name.
+     * @throws Exception If an error occurs while fetching the name.
+     */
 
     @Override
     protected String onGetName() throws Exception {
         return player.getName();
     }
 
+    /**
+     * Verifies the game state with the scores received from the server.
+     *
+     * @param immutableList The scores of all players in the game.
+     * @throws Exception If an error occurs during game verification.
+     */
+    
     @Override
     protected void onVerifyGame(ImmutableList<Integer> immutableList) throws Exception {
         GameBoard.graphics.update(true);
