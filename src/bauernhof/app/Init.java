216fc62da265635d08bc63adc84a13e03ce9be2e
PaintableToast <rckimyon@yahoo.com>
2023-07-24 09:06:09 +0200

    Init

--- src/bauernhof/app/Init.java
+++ src/bauernhof/app/Init.java
@@ -1,7 +1,5 @@
 package bauernhof.app;
 
-import java.awt.*;
-import java.io.File;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
@@ -14,45 +12,30 @@ import bauernhof.preset.*;
 import bauernhof.preset.networking.S2CConnection;
 
 public class Init {
-    public static void main(String[] args) throws Exception {
-        List<String> names = new ArrayList<>(List.of("Kirill Pokhilenko", "Ramon Cemil Kimyon", "Viktor Tevosyan","Florian Will", "Julius Hunold"));
-        List<OptionalFeature> optionalFeatures = new ArrayList<>(List.of(OptionalFeature.ADVANCED_AI, OptionalFeature.LAUNCHER, OptionalFeature.SAVEGAMES, OptionalFeature.SCREENSHOTS, OptionalFeature.SIMPLE_AI, OptionalFeature.SOUNDEFFECTS, OptionalFeature.TOURNAMENTS));
-        if (args.length == 0) {
-            args = new String[1];
-            args[0] = "-l";
-        }
-        Settings settings = new ArgumentParser(args, "Hofbauern", "1.2.1", names, optionalFeatures);
 
-        GaCoPa gacopa = new GaCoPa();
-        /*if (settings.shouldLauncherLaunch)
-            new InitLauncher();*/
-        settings.delay = 1000;
-        settings.showGUI = true;
-        settings.volume = 0;
-        settings.logLevel = LogLevel.INFO;
-        settings.playerNames = List.of(new String[]{"REMOTE", "Player 2", "Player 3", "Wasser"});
-        settings.playerColors = List.of(new Color[]{Color.RED, Color.GREEN, Color.YELLOW, Color.PINK});
-        settings.playerTypes = List.of(new PlayerType[]{PlayerType.HUMAN, PlayerType.RANDOM_AI, PlayerType.RANDOM_AI, PlayerType.RANDOM_AI});
-        settings.gameConfigurationFile = new File("gameconfigs/bauernhof.xml");
-        settings.delay = 50L;
-        settings.showGUI = true;
-        settings.connectToHostname = null;
-        settings.port = 6600;
-        settings.loadSaveGameFile = null;
-        settings.shouldLauncherLaunch = false;
-        settings.numTournamentRounds = 0;
-        settings.waitAfterTournamentRound = false;
-        settings.volume = 0;
-        Init.initGame(NetworkGetSettings.getSettings(0));
+    public static void main(String[] args) throws Exception {
+        List<String> names = new ArrayList<>(List.of("Kirill Rokhilenko", "Ramon Cemil Kimyon", "Viktor Tevosyan", "Florian Will", "Julius Hunold"));
+        List<OptionalFeature> optionalFeatures = new ArrayList<>(List.of(
+                OptionalFeature.ADVANCED_AI,
+                OptionalFeature.LAUNCHER,
+                OptionalFeature.SCREENSHOTS,
+                OptionalFeature.SIMPLE_AI,
+                OptionalFeature.TOURNAMENTS));
+        final Settings settings = new ArgumentParser(args, "Hofbauern", "1.2.1", names, optionalFeatures);
+        if (settings.shouldLauncherLaunch)
+            new InitLauncher();
+        else initGame(settings);
     }
     public static void initGame(final Settings settings) throws Exception {
         final GaCoPa gacopa = new GaCoPa();
         ArrayList<S2CConnection> connections = new ArrayList<>();
+        ServerSocket socket;
         if(settings.numTournamentRounds == 0) {
-            if (settings.connectToHostname != null) new Client(settings, new Socket(settings.connectToHostname, settings.port), gacopa, "Hofbauern");
+            if (settings.connectToHostname != null)
+                new Client(settings, new Socket(settings.connectToHostname, settings.port), gacopa, "Hofbauern");
             else {
                 if (settings.playerTypes.contains(PlayerType.REMOTE)) {
-                    final ServerSocket socket = new ServerSocket(settings.port);
+                    socket = new ServerSocket(settings.port);
                     for (final PlayerType type : settings.playerTypes)
                         if (type.equals(PlayerType.REMOTE))
                             connections.add(new S2CConnection(socket.accept()));
